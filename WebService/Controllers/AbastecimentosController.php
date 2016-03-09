1970b6ae6d685b9cb66a3c88706593c824f786f5
Edwino Stein <edwino.stein@gmail.com>
2016-03-08 23:39:48 -0400

    Adicionando createAction ao AbastecimetnosController

--- WebService/Controllers/AbastecimentosController.php
+++ WebService/Controllers/AbastecimentosController.php
@@ -43,5 +43,63 @@ class AbastecimentosController {
         ));
     }
 
+    /**
+     * Cria novos registros para a tabela abastecimentos.
+     */
+    public function createAction(){
+
+        //Mapeia a entradas de dados
+        $inputMap = array('valorTotal', 'litros', 'precoLitro', 'quilometragem', 'data');
+        $data = array();
+        $erros = array();
+        $value = null;
+
+        //Procura os dados informados a partir do mapa de entrada
+        foreach ($inputMap as $key) {
+            $value = Application::getParam($key);
+            if($value === null)
+                $erros[] = $key;
+            else
+                $data[$key] = $value;
+        }
+
+        //Retorna um erro para caso um campo tenha faltado
+        if(!empty($erros)){
+            return json_encode(array(
+                'success' => false,
+                'message' => 'Alguns campos estão vazios ou são inválidos.',
+                'extra' => $erros,
+                'code' => 0
+            ));
+        }
+
+        //Instancia e inicializa uma model
+        $model = new Abastecimento();
+        $model->setValorTotal($data['valorTotal']);
+        $model->setLitros($data['litros']);
+        $model->setPrecoLitro($data['precoLitro']);
+        $model->setQuilometragem($data['quilometragem']);
+        $model->setData($data['data']);
+        $model->setCriadoEm(new \DateTime());
+
+        try{
+            //Salva os dados no banco de dados
+            $model->save();
+        }
+        catch(\Exception $e){
+
+            //Caso algo dê errado, retorna uma menssagem de erro
+            return json_encode(array(
+                'success' => false,
+                'message' => $e->getMessage(),
+                'code' => $e->getCode()
+            ));
+        }
+
+        //Retorna os dados que foram pessistidos no banco de dados
+        return json_encode(array(
+            'success' => true,
+            'data' => $model->toArray()
+        ));
     }
 }
