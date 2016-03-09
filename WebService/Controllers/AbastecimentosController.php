ab74f199a0d7651f43a1dab548ac457eeac652e6
Edwino Stein <edwino.stein@gmail.com>
2016-03-08 23:41:15 -0400

    Adicionando updateAction ao AbastecimentosController

--- WebService/Controllers/AbastecimentosController.php
+++ WebService/Controllers/AbastecimentosController.php
@@ -102,4 +102,84 @@ class AbastecimentosController {
             'data' => $model->toArray()
         ));
     }
+
+    /**
+     * Atualiza os registros da tabela abastecimentos.
+     */
+    public function updateAction(){
+
+        //Mapeia a entradas de dados
+        $inputMap = array('id', 'valorTotal', 'litros', 'precoLitro', 'quilometragem', 'data');
+        $data = array();
+        $erros = array();
+        $value = null;
+
+        //Procura os dados informados a partir do mapa de entrada
+        foreach ($inputMap as $key) {
+            $value = Application::getParam($key);
+            if($value === null)
+                $erros[] = $key;
+            else
+                $data[$key] = $value;
+        }
+
+        //Retorna um erro para caso um campo tenha faltado
+        if(!empty($erros)){
+            return json_encode(array(
+                'success' => false,
+                'message' => 'Alguns campos estão vazios ou são inválidos.',
+                'extra' => $erros,
+                'code' => 0
+            ));
+        }
+
+        try {
+            //Busta o registro especificado para a atualização
+            $model = Abastecimento::findOneBy(array('id' => $data['id']));
+
+        } catch (Exception $e) {
+
+            //Em caso de erro, retorna uma menssagem de erro
+            return json_encode(array(
+                'success' => false,
+                'message' => 'O abastecimento é inválido ou não existe.'
+            ));
+        }
+
+        //Caso não a encontre, retorna uma menssagem de erro.
+        if($model === null){
+            return json_encode(array(
+                'success' => false,
+                'message' => 'O abastecimento é inválido ou não existe.'
+            ));
+        }
+
+        //Atualiza os dados da model
+        $model->setValorTotal($data['valorTotal']);
+        $model->setLitros($data['litros']);
+        $model->setPrecoLitro($data['precoLitro']);
+        $model->setQuilometragem($data['quilometragem']);
+        $model->setData($data['data']);
+
+        try{
+            //Salva os dados no banco de dados
+            $model->save();
+        }
+        catch(\Exception $e){
+
+            //Caso algo dê errado, retorna uma menssagem de erro
+            return json_encode(array(
+                'success' => false,
+                'message' => $e->getMessage(),
+                'code' => $e->getCode()
+            ));
+        }
+
+        //Retorna os dados que foram pessistidos no banco de dados
+        return json_encode(array(
+            'success' => true,
+            'data' => $model->toArray()
+        ));
+    }
+
 }
